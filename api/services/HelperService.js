b094ead920016ccf79902604c3b6df581d01f392
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2015-11-14 02:00:46 +0100

    handling of relative reddit specific links improved

--- api/services/HelperService.js
+++ api/services/HelperService.js
@@ -4,6 +4,7 @@ module.exports = {
     if (html.indexOf('<p>[removed]</p>') != -1 || html.indexOf('<p>[deleted]</p>') != -1) {
       return false;
     }
-    return html.replace('/u/', 'http://reddit.com/u/').replace('/r/', 'http://reddit.com/r/');
+    // replace instances where links refer to /u/... or /r/ with actual links to reddit
+    return html.replace(/href=\"\/u\//g, 'href=\"https://reddit.com/u/').replace(/href=\"\/r\//g, 'href=\"https://reddit.com/r/');
   }
 };
